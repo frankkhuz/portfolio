9b453161bbef64e0a08e69d4c2e33ebc039823ac
Franklin <frankkhuzy@gmail.com>
2023-08-17 01:28:25 -0700

    first commit

---
+++ j.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const n = document.querySelector('.navMenue');
+
+hamburger.addEventListener('click', () => {
+hamburger.classList.toggle('active')
+navMenu.classList.toggle('active')
+})
