c79c1aba217a4dcbd69e9e56da129c54eea9bca4
Franklin <frankkhuzy@gmail.com>
2023-08-19 13:03:27 -0700

    .

--- j.js
+++ j.js
@@ -1,7 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
-const n = document.querySelector('.navMenue');
+const navMenu = document.querySelector('.navMenu');
 
 hamburger.addEventListener('click', () => {
 hamburger.classList.toggle('active')
 navMenu.classList.toggle('active')
 })
+document.querySelectorAll(".navMenu").forEach(n => n.addEventListener("click", ()=> {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active")
+}));
