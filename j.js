dbc3053ef31115d7f0e2fbdbde17781e5a06b423
Franklin <frankkhuzy@gmail.com>
2023-11-05 10:46:28 -0800

    .

--- j.js
+++ j.js
@@ -8,4 +8,9 @@ navMenu.classList.toggle('active')
 document.querySelectorAll(".navMenu").forEach(n => n.addEventListener("click", ()=> {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active")
-}));
+})); 
+
+themeToggleBtn = document.querySelector('.toggleDarkMode');
+themeToggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode')
+});
